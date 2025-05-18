0c2e0e20d358de91a9a5272ee6204d2dc5115375
Toporio <grzesiotoporski@gmail.com>
2025-05-18 20:58:07 +0200

    add config

---
+++ include/config.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+
+namespace Config {
+    const unsigned int WINDOW_WIDTH = 1280;
+    const unsigned int WINDOW_HEIGHT= 720;
+    const std::string WINDOW_TITLE = "SkyRift";
+
+}
