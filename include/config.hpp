8b43d0ad5728fbe4f5a0d31ab99223e8d90e2f67
Toporio <grzesiotoporski@gmail.com>
2025-05-19 23:52:42 +0200

    added fps_limit

--- include/config.hpp
+++ include/config.hpp
@@ -1,10 +1,9 @@
 #pragma once
 #include <string>
 
-
 namespace Config {
-    const unsigned int WINDOW_WIDTH = 1280;
-    const unsigned int WINDOW_HEIGHT= 720;
-    const std::string WINDOW_TITLE = "SkyRift";
-
-}
+const unsigned int WINDOW_WIDTH = 1280;
+const unsigned int WINDOW_HEIGHT = 720;
+const std::string WINDOW_TITLE = "SkyRift";
+const unsigned int FPS_LIMIT = 144;
+} // namespace Config
