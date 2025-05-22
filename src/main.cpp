ab701422e34330cb5ce469b25e293b2453033082
Adam Piasecki <01197513@pw.edu.pl>
2025-05-22 11:51:36 +0200

    test numero uno of tile and map

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "Game.hpp"
+#include "Map.hpp"
+#include "Tile.hpp"
 #include <SFML/Graphics.hpp>
 #include <cerrno>
 #include <cstdlib>
@@ -8,6 +10,16 @@ int main() {
   try {
     Game game;
     game.run();
+    sf::Texture rock1("Rock1.png");
+    Tile tile1(1, rock1, sf::Vector2f(100.0f, 100.0f));
+    sf::Texture rock2("Rock2.png");
+    Tile tile2(2, rock2, sf::Vector2f(230.0f, 280.0f));
+    Map map1();
+    map1.add_tile(tile1);
+    map1.add_tile(tile2);
+    map1.draw(game.window);
+    game.window.display();
+
   } catch (const std::exception &e) {
     std::cerr << "Error: " << e.what() << std::endl;
     return EXIT_FAILURE;
