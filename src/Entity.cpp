a5692718144829b964984f274012a42866dac137
Toporio <grzesiotoporski@gmail.com>
2025-05-26 22:37:49 +0200

    zajebiste assety mordo

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -4,9 +4,13 @@
 
 Entity::Entity(int id, const sf::Texture &texture, const sf::Vector2f &position,
                const sf::Vector2f &velocity)
-    : id(id), position(position), velocity(velocity), sprite(texture) {}
+    : id(id), position(position), velocity(velocity), sprite(texture) {
+  sprite.setPosition(position);
+}
 
-bool Entity::check_collision(const Entity& other) const {
-    return (this->sprite.getGlobalBounds().findIntersection(other.sprite.getGlobalBounds())).has_value();
+bool Entity::check_collision(const Entity &other) const {
+  return (this->sprite.getGlobalBounds().findIntersection(
+              other.sprite.getGlobalBounds()))
+      .has_value();
 }
 void Entity::draw(sf::RenderWindow &window) const { window.draw(sprite); }
